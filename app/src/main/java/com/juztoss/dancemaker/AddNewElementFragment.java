aec448553ba8b47761d48329428da2a648f5c458
juztoss <juztoss@gmail.com>
2016-02-27 14:10:04 +0300

    first commit

---
+++ app/src/main/java/com/juztoss/dancemaker/AddNewElementFragment.java
@@ -0,0 +1,19 @@
+package com.juztoss.dancemaker;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kirill on 2/27/2016.
+ */
+public class AddNewElementFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.add_new_view, container, false);
+    }
+}
